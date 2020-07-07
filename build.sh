ac2dfd2689ea851b2df94647701607c876efe449
François Génolini <francois.genolini@gmail.com>
2020-07-07 14:43:34 +0100

    travis fixes
    travis build failed, adding fixes

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+pwd
 mkdir -p build
 cd build
 # cmake ${CMAKE_OPTIONS} -DCMAKE_CXX_FLAGS=${CXX_FLAGS} -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTS=ON ..
-cmake ${CMAKE_OPTIONS} -DCMAKE_CXX_FLAGS=${CXX_FLAGS} -DCMAKE_BUILD_TYPE=Release
+cmake ${CMAKE_OPTIONS} -DCMAKE_CXX_FLAGS=${CXX_FLAGS} -DCMAKE_BUILD_TYPE=Release ..
 make
 # make frank_coverage
 # ctest
--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -5,3 +5,19 @@ sudo apt-get remove x264 libx264-dev -y
 # In the .travis.yml file:
 # sudo apt-get install -y build-essential checkinstall cmake pkg-config git libopencv-dev python3-opencv
 
+pwd
+curl -sL https://github.com/microsoft/GSL/archive/master.zip > GSL.zip
+unzip -q GSL.zip
+
+cd GSL-master
+mkdir -p build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_CXX_COMPILER=g++-9 \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      ..
+make
+sudo make install
+cd ..
+cd ..
+pwd
