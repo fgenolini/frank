69b34b29cd70d0b6fa7a81f0fb516528a76cd3f7
François Génolini <francois.genolini@gmail.com>
2020-07-07 13:16:27 +0100

    Update video_main.cpp
    formatting and continue execution even if list_input_devices() fails

--- video/video_main.cpp
+++ video/video_main.cpp
@@ -1,14 +1,12 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "videoConfig.h"
 #include "list_devices.h"
 #include "opencv_ui.h"
+#include "videoConfig.h"
 
-int main(int argc, char const *argv[])
-{
-  if (argc < 2)
-  {
+int main(int argc, char const *argv[]) {
+  if (argc < 2) {
     std::cout << argv[0] << " v" << VIDEO_VERSION_MAJOR << "."
               << VIDEO_VERSION_MINOR << "\n";
     std::cout << "Usage: " << argv[0] << " number"
@@ -16,9 +14,9 @@ int main(int argc, char const *argv[])
     exit(EXIT_FAILURE);
   }
 
-  if (!frank::video::list_input_devices())
-  {
-    exit(EXIT_FAILURE);
+  if (!frank::video::list_input_devices()) {
+    std::cerr << "Could not list video / audio input devices"
+              << "\n";
   }
 
   frank::video::opencv_ui();
