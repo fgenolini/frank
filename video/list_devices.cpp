f6d6820c4a0aa89ba7c97f24a09d0c3b508fcdee
François Génolini <francois.genolini@gmail.com>
2020-07-07 13:16:00 +0100

    Update list_devices.cpp
    formatting and default dummy implementation of list_input_devices() for non-Windows platforms

--- video/list_devices.cpp
+++ video/list_devices.cpp
@@ -1,5 +1,8 @@
-#include <windows.h>
+#include "config.h"
+
+#if defined(HAVE_WINDOWS_H)
 #include <dshow.h>
+#include <windows.h>
 
 #include <gsl/gsl_util>
 
@@ -9,135 +12,122 @@ using namespace gsl;
 
 #pragma comment(lib, "strmiids")
 
-namespace frank::video
-{
+namespace frank::video {
 
-  HRESULT devices_from_category(REFGUID category, IEnumMoniker **devices)
-  {
-    ICreateDevEnum *system_device{};
-    auto class_creator = CoCreateInstance(CLSID_SystemDeviceEnum, nullptr,
-                                          CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&system_device));
-    if (FAILED(class_creator))
-    {
-      printf("Could not create system device enumerator\n");
-      return class_creator;
-    }
-
-    auto _ = finally([system_device] {
-      system_device->Release();
-    });
-
-    auto category_enumerator = system_device->CreateClassEnumerator(category, devices, 0);
-    if (category_enumerator == S_FALSE)
-    {
-      printf("Empty category\n");
-      category_enumerator = VFW_E_NOT_FOUND;
-    }
-
-    return category_enumerator;
+HRESULT devices_from_category(REFGUID category, IEnumMoniker **devices) {
+  ICreateDevEnum *system_device{};
+  auto class_creator =
+      CoCreateInstance(CLSID_SystemDeviceEnum, nullptr, CLSCTX_INPROC_SERVER,
+                       IID_PPV_ARGS(&system_device));
+  if (FAILED(class_creator)) {
+    printf("Could not create system device enumerator\n");
+    return class_creator;
   }
 
-  void device_information(IEnumMoniker *devices)
-  {
-    IMoniker *moniker{};
-    while (devices->Next(1, &moniker, nullptr) == S_OK)
-    {
-      auto _ = finally([moniker] {
-        moniker->Release();
-      });
-      IPropertyBag *property_bag{};
-      auto bind_to_storage = moniker->BindToStorage(0, 0, IID_PPV_ARGS(&property_bag));
-      if (FAILED(bind_to_storage))
-      {
-        printf("Could not bind to storage\n");
-        continue;
-      }
+  auto _ = finally([system_device] { system_device->Release(); });
 
-      auto _1 = finally([property_bag] {
-        property_bag->Release();
-      });
-      VARIANT property;
-      VariantInit(&property);
-      auto get_description = property_bag->Read(L"Description", &property, 0);
-      if (FAILED(get_description))
-      {
-        get_description = property_bag->Read(L"FriendlyName", &property, 0);
-      }
-
-      if (SUCCEEDED(get_description))
-      {
-        printf("%S\n", property.bstrVal);
-      }
-
-      VariantClear(&property);
-      auto get_audio_id = property_bag->Read(L"WaveInID", &property, 0);
-      if (SUCCEEDED(get_audio_id))
-      {
-        printf("  WaveIn ID: %d\n", property.lVal);
-      }
-
-      VariantClear(&property);
-      auto get_device_path = property_bag->Read(L"DevicePath", &property, 0);
-      if (SUCCEEDED(get_device_path))
-      {
-        printf("  Device path: %S\n", property.bstrVal);
-      }
-
-      VariantClear(&property);
-    }
+  auto category_enumerator =
+      system_device->CreateClassEnumerator(category, devices, 0);
+  if (category_enumerator == S_FALSE) {
+    printf("Empty category\n");
+    category_enumerator = VFW_E_NOT_FOUND;
   }
 
-  void audio_devices()
-  {
-    printf("Audio input devices:\n");
-    IEnumMoniker *audio_devices{};
-    auto enumerate = devices_from_category(CLSID_AudioInputDeviceCategory, &audio_devices);
-    if (FAILED(enumerate))
-    {
-      printf("Could not list audio input devices\n");
-      return;
+  return category_enumerator;
+}
+
+void device_information(IEnumMoniker *devices) {
+  IMoniker *moniker{};
+  while (devices->Next(1, &moniker, nullptr) == S_OK) {
+    auto _ = finally([moniker] { moniker->Release(); });
+    IPropertyBag *property_bag{};
+    auto bind_to_storage =
+        moniker->BindToStorage(0, 0, IID_PPV_ARGS(&property_bag));
+    if (FAILED(bind_to_storage)) {
+      printf("Could not bind to storage\n");
+      continue;
     }
 
-    auto _ = finally([audio_devices] {
-      audio_devices->Release();
-    });
-    device_information(audio_devices);
-  }
-
-  bool video_devices()
-  {
-    printf("Video input devices:\n");
-    IEnumMoniker *video_devices{};
-    auto enumerate = devices_from_category(CLSID_VideoInputDeviceCategory, &video_devices);
-    if (FAILED(enumerate))
-    {
-      printf("Could not list video input devices\n");
-      return false;
+    auto _1 = finally([property_bag] { property_bag->Release(); });
+    VARIANT property;
+    VariantInit(&property);
+    auto get_description = property_bag->Read(L"Description", &property, 0);
+    if (FAILED(get_description)) {
+      get_description = property_bag->Read(L"FriendlyName", &property, 0);
     }
 
-    auto _ = finally([video_devices] {
-      video_devices->Release();
-    });
-    device_information(video_devices);
-    return true;
-  }
-
-  bool list_input_devices()
-  {
-    auto com = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
-    if (FAILED(com))
-    {
-      printf("Could not initialise COM\n");
-      return false;
+    if (SUCCEEDED(get_description)) {
+      printf("%S\n", property.bstrVal);
     }
 
-    auto _ = finally([] {
-      CoUninitialize();
-    });
-    auto video = video_devices();
-    printf("\n");
-    audio_devices();
-    return video;
+    VariantClear(&property);
+    auto get_audio_id = property_bag->Read(L"WaveInID", &property, 0);
+    if (SUCCEEDED(get_audio_id)) {
+      printf("  WaveIn ID: %d\n", property.lVal);
+    }
+
+    VariantClear(&property);
+    auto get_device_path = property_bag->Read(L"DevicePath", &property, 0);
+    if (SUCCEEDED(get_device_path)) {
+      printf("  Device path: %S\n", property.bstrVal);
+    }
+
+    VariantClear(&property);
   }
+}
+
+void audio_devices() {
+  printf("Audio input devices:\n");
+  IEnumMoniker *audio_devices{};
+  auto enumerate =
+      devices_from_category(CLSID_AudioInputDeviceCategory, &audio_devices);
+  if (FAILED(enumerate)) {
+    printf("Could not list audio input devices\n");
+    return;
+  }
+
+  auto _ = finally([audio_devices] { audio_devices->Release(); });
+  device_information(audio_devices);
+}
+
+bool video_devices() {
+  printf("Video input devices:\n");
+  IEnumMoniker *video_devices{};
+  auto enumerate =
+      devices_from_category(CLSID_VideoInputDeviceCategory, &video_devices);
+  if (FAILED(enumerate)) {
+    printf("Could not list video input devices\n");
+    return false;
+  }
+
+  auto _ = finally([video_devices] { video_devices->Release(); });
+  device_information(video_devices);
+  return true;
+}
+
+bool list_input_devices() {
+  auto com = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+  if (FAILED(com)) {
+    printf("Could not initialise COM\n");
+    return false;
+  }
+
+  auto _ = finally([] { CoUninitialize(); });
+  auto video = video_devices();
+  printf("\n");
+  audio_devices();
+  return video;
+}
 
 } // namespace frank::video
+
+#else
+
+// Non-Windows implementations of video/audio device enumeration
+// POSIX, Linux, Mac OSX...
+
+namespace frank::video {
+bool list_input_devices() { return false; }
+} // namespace frank::video
+
+#endif
