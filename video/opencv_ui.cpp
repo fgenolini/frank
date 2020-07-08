c660fa54f6732821ad76e720fa4d833efa080112
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-08 16:19:04 +0100

    use webcam as window background
    use the first available video input device as the background image for the OpenCV window, displaying live video frames.
    Convert test for escape key into a lambda function that can be reused when attempting to open the webcam

--- video/opencv_ui.cpp
+++ video/opencv_ui.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 #define CVUI_IMPLEMENTATION
 #include "cvui.h"
@@ -11,18 +12,40 @@
 namespace frank::video {
 
 [[noreturn]] void opencv_ui() {
-  constexpr auto ESCAPE_KEY = 27;
-  constexpr auto SHORT_DELAY = 20;
   constexpr auto VIDEO_COUNT = 4;
   constexpr auto WINDOW_NAME = "Frank video";
 
+  auto wait_or_exit = []() {
+    constexpr auto ESCAPE_KEY = 27;
+    constexpr auto SHORT_DELAY = 20;
+
+    if (cv::waitKey(SHORT_DELAY) == ESCAPE_KEY) {
+      exit(EXIT_SUCCESS);
+    }
+  };
+
+  auto take_picture = [wait_or_exit](cv::VideoCapture &webcam) {
+    while (!webcam.isOpened()) {
+      std::cout << "Could not open webcam" << '\n';
+      int index{};
+      webcam.open(index, cv::CAP_ANY);
+      wait_or_exit();
+    }
+
+    cv::Mat pic;
+    webcam >> pic;
+    return pic;
+  };
+
   bool use_video[VIDEO_COUNT]{};
+  cv::VideoCapture input_video_device(0);
   auto frame = cv::Mat(200, 500, CV_8UC3);
   EnhancedWindow settings(200, 50, 200, 100, "Settings");
 
   cvui::init(WINDOW_NAME);
   while (true) {
     frame = cv::Scalar(49, 52, 49);
+    frame = take_picture(input_video_device);
     auto button_clicked = cvui::button(frame, 110, 80, "Quit");
     if (button_clicked) {
       exit(EXIT_SUCCESS);
@@ -39,9 +62,7 @@ namespace frank::video {
     settings.end();
 
     cvui::imshow(WINDOW_NAME, frame);
-    if (cv::waitKey(SHORT_DELAY) == ESCAPE_KEY) {
-      exit(EXIT_SUCCESS);
-    }
+    wait_or_exit();
   }
 
   exit(EXIT_SUCCESS);
