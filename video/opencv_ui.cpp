38e5169a1c9d72d1bf0ed0e5194906d318761aa6
Francois Genolini <2612722+fgenolini@users.noreply.github.com>
2020-07-08 15:39:17 +0100

    Update opencv_ui.cpp
    added settings window using cvui to allow user to see 4 checkboxes that will represent video input displays being turned on or off

--- video/opencv_ui.cpp
+++ video/opencv_ui.cpp
@@ -4,6 +4,7 @@
 
 #define CVUI_IMPLEMENTATION
 #include "cvui.h"
+#include "EnhancedWindow.h"
 
 #include "opencv_ui.h"
 
@@ -12,17 +13,30 @@ namespace frank::video {
 [[noreturn]] void opencv_ui() {
   constexpr auto ESCAPE_KEY = 27;
   constexpr auto SHORT_DELAY = 20;
+  constexpr auto VIDEO_COUNT = 4;
   constexpr auto WINDOW_NAME = "Frank video";
 
+  bool use_video[VIDEO_COUNT]{};
   auto frame = cv::Mat(200, 500, CV_8UC3);
+  EnhancedWindow settings(200, 50, 200, 100, "Settings");
+
   cvui::init(WINDOW_NAME);
   while (true) {
-    // Fill the frame with a nice color
     frame = cv::Scalar(49, 52, 49);
     auto button_clicked = cvui::button(frame, 110, 80, "Quit");
     if (button_clicked) {
       exit(EXIT_SUCCESS);
     }
+
+    settings.begin(frame);
+    if (!settings.isMinimized()) {
+      for (auto i = 0; i < VIDEO_COUNT; ++i) {
+        std::string video_name{"Video " + std::to_string(i)};
+        cvui::checkbox(video_name, &use_video[i]);
+      }
+    }
+
+    settings.end();
 
     cvui::imshow(WINDOW_NAME, frame);
     if (cv::waitKey(SHORT_DELAY) == ESCAPE_KEY) {
