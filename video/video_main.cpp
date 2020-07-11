e2109f584ae4a5e10de1b9fdbb8c804178dda933
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-11 21:46:10 +0100

    run application and tests from separate entry points
    catch unhandled exceptions when running tests

--- video/video_main.cpp
+++ video/video_main.cpp
@@ -2,12 +2,13 @@
 #include <cstring>
 #include <iostream>
 
-#include "list_devices.h"
-#include "opencv_ui.h"
+#include "run_application.h"
 #include "run_tests.h"
 #include "videoConfig.h"
 
 int main(int argc, char const *argv[]) {
+  constexpr auto TEST_ARGUMENT = "--test";
+
   if (argc < 2) {
     std::cout << argv[0] << " v" << VIDEO_VERSION_MAJOR << "."
               << VIDEO_VERSION_MINOR << "\n";
@@ -16,15 +17,9 @@ int main(int argc, char const *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  if (strcmp(argv[1], "test") == 0) {
+  if (strcmp(argv[1], TEST_ARGUMENT) == 0) {
     frank::video::run_tests(argc, argv);
   }
 
-  auto input_devices = frank::video::list_input_devices();
-  if (input_devices.size() < 1) {
-    std::cerr << "Could not list video / audio input devices"
-              << "\n";
-  }
-
-  frank::video::opencv_ui(input_devices);
+  frank::video::run_application(argc, argv);
 }
