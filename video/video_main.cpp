2fd8b2082691f80afd8fa9c07565dcc40de9c9de
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-17 23:03:21 +0100

    Update video_main.cpp
    cleanup, to allow intalled software to run when clicked, as opposed to being used from the command line and passing arguments

--- video/video_main.cpp
+++ video/video_main.cpp
@@ -6,12 +6,7 @@
 #include "videoConfig.h"
 
 int main(int argc, char const *argv[]) {
-  if (argc < 2) {
-    std::cerr << argv[0] << " v" << VIDEO_VERSION_MAJOR << "."
-              << VIDEO_VERSION_MINOR << '\n';
-    std::cerr << "Usage: " << argv[0] << " number_of_video_inputs\n";
-    exit(EXIT_FAILURE);
-  }
-
+  std::cerr << argv[0] << " v" << VIDEO_VERSION_MAJOR << "."
+            << VIDEO_VERSION_MINOR << '\n';
   frank::video::run_application(argc, argv);
 }
