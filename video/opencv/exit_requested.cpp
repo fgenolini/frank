d67786adc9600798aaba8558d3d5b0da830804cd
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-24 23:16:33 +0100

    Update exit_requested.cpp
    minor clang-format reformatting, remove needless semicolon

--- video/opencv/exit_requested.cpp
+++ video/opencv/exit_requested.cpp
@@ -4,11 +4,11 @@
 
 namespace frank::video {
 
-bool exit_requested () {
+bool exit_requested() {
   constexpr auto ESCAPE_KEY = 27;
   constexpr auto SHORT_DELAY = 20;
 
   return cv::waitKey(SHORT_DELAY) == ESCAPE_KEY;
-};
+}
 
 } // namespace frank::video
