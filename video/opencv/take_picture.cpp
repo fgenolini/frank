75b57c7e06e27f29869063a8cb25f9cd46bfbd8d
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-25 20:42:47 +0100

    reduce_resolution in take_picture
    if the current live video input (webcam) resolution is too large, reduce it so that there is less screen real estate taken by the 4 video inputs, and to improve the speed of OpenCV data processing on each frame, so that the live frame rate does not drop too much due to underpowered CPU

--- video/opencv/take_picture.cpp
+++ video/opencv/take_picture.cpp
@@ -9,6 +9,35 @@
 
 namespace frank::video {
 
+void reduce_resolution(cv::VideoCapture *webcam, int webcam_index) {
+  constexpr auto MAXIMUM_HEIGHT_0 = 360.0;
+  constexpr auto MAXIMUM_HEIGHT_OTHER = 240.0;
+  auto webcam_height = webcam->get(cv::CAP_PROP_FRAME_HEIGHT);
+  auto webcam_width = webcam->get(cv::CAP_PROP_FRAME_WIDTH);
+  std::cout << "Resolution #" << webcam_index << " h:" << webcam_height
+            << ", w:" << webcam_width << '\n';
+  auto maximum_height = MAXIMUM_HEIGHT_OTHER;
+  if (webcam_index == 0) {
+    maximum_height = MAXIMUM_HEIGHT_0;
+  }
+
+  if (webcam_height <= maximum_height) {
+    return;
+  }
+
+  auto new_height = maximum_height;
+  auto ratio = webcam_height / maximum_height;
+  auto new_width = webcam_width / ratio;
+  std::cout << "Proposed reduced resolution h:" << new_height
+            << ", w:" << new_width << '\n';
+  webcam->set(cv::CAP_PROP_FRAME_HEIGHT, new_height);
+  webcam->set(cv::CAP_PROP_FRAME_WIDTH, new_width);
+  webcam_height = webcam->get(cv::CAP_PROP_FRAME_HEIGHT);
+  webcam_width = webcam->get(cv::CAP_PROP_FRAME_WIDTH);
+  std::cout << "Agreed new resolution h:" << webcam_height
+            << ", w:" << webcam_width << '\n';
+}
+
 cv::Mat take_picture(opencv_window &window) {
   cv::Mat pic{};
   auto webcam = window.webcam();
@@ -23,6 +52,7 @@ cv::Mat take_picture(opencv_window &window) {
       std::cout << "Could not open webcam " << webcam_index << '\n';
     }
 
+    reduce_resolution(webcam, webcam_index);
     if (exit_requested()) {
       window.set_exit_requested(true);
       return pic;
