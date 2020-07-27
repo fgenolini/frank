57f367f08096c0640aa4235b2ad176f426d364c2
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-27 13:34:58 +0100

    refactor opencv_with_webcams into video_gui class
    use RAII instead of gsl::finally to release webcams, a more idiomatic C++ implementation

--- video/ui/opencv_ui.cpp
+++ video/ui/opencv_ui.cpp
@@ -3,10 +3,6 @@
 #include <memory>
 #include <utility>
 
-#include <gsl/gsl_util>
-
-using namespace gsl;
-
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
 
@@ -22,21 +18,49 @@ using namespace gsl;
 
 namespace frank::video {
 
-bool opencv_with_webcams(std::vector<input_device> &connected_webcams) {
-  constexpr auto MAXIMUM_VIDEO_COUNT = 4;
-  constexpr auto WINDOW_NAME = "Frank video";
-  constexpr auto WINDOW1_NAME = "Frank video 1";
-  constexpr auto WINDOW2_NAME = "Frank video 2";
-  constexpr auto WINDOW3_NAME = "Frank video 3";
+constexpr auto MAXIMUM_VIDEO_COUNT = 4;
+constexpr auto SETTINGS_HEIGHT = 280;
+constexpr auto SETTINGS_TITLE = "Settings";
+constexpr auto SETTINGS_WIDTH = 600;
+constexpr auto SETTINGS_X = 10;
+constexpr auto SETTINGS_Y = 50;
+constexpr auto WINDOW_NAME = "Frank video";
+constexpr auto WINDOW1_NAME = "Frank video 1";
+constexpr auto WINDOW2_NAME = "Frank video 2";
+constexpr auto WINDOW3_NAME = "Frank video 3";
 
+class video_gui {
+public:
+  video_gui(std::vector<input_device> &connected_webcams);
+  ~video_gui();
+
+  bool loop(std::vector<input_device> &connected_webcams);
+
+  EnhancedWindow settings{SETTINGS_X, SETTINGS_Y, SETTINGS_WIDTH,
+                          SETTINGS_HEIGHT, SETTINGS_TITLE};
+  opencv_window *window_template;
   std::vector<cv::String> overlay_images{};
-  std::vector<cv::String> window_names = {WINDOW_NAME, WINDOW1_NAME,
-                                          WINDOW2_NAME, WINDOW3_NAME};
+  std::vector<cv::String> window_names{WINDOW_NAME, WINDOW1_NAME, WINDOW2_NAME,
+                                       WINDOW3_NAME};
   std::vector<std::pair<double, double>> height_width_pairs{};
+  std::vector<std::unique_ptr<cv::VideoCapture>> input_video_devices{};
+  std::vector<bool> has_webcams{};
   double overlay_alpha[MAXIMUM_VIDEO_COUNT]{};
   bool overlay_enabled[MAXIMUM_VIDEO_COUNT]{};
   bool video_enabled[MAXIMUM_VIDEO_COUNT]{};
-  std::vector<bool> has_webcams{};
+};
+
+video_gui::~video_gui() {
+  std::cout << "Releasing webcams" << '\n';
+  for (auto &video_capture : input_video_devices) {
+    auto capturing_device = video_capture.get();
+    capturing_device->release();
+  }
+
+  delete window_template;
+}
+
+video_gui::video_gui(std::vector<input_device> &connected_webcams) {
   for (auto webcam = 0;
        webcam < (int)window_names.size() && webcam < MAXIMUM_VIDEO_COUNT;
        ++webcam) {
@@ -51,21 +75,12 @@ bool opencv_with_webcams(std::vector<input_device> &connected_webcams) {
     has_webcams[0] = true;
   }
 
-  std::vector<std::unique_ptr<cv::VideoCapture>> input_video_devices{};
   for ([[maybe_unused]] auto _ : has_webcams) {
     auto input_video_device = std::make_unique<cv::VideoCapture>();
     input_video_devices.push_back(std::move(input_video_device));
     overlay_images.push_back("");
     height_width_pairs.push_back(std::make_pair(0.0, 0.0));
   }
-
-  auto _ = finally([&input_video_devices] {
-    std::cout << "Releasing webcams" << '\n';
-    for (auto &video_capture : input_video_devices) {
-      auto capturing_device = video_capture.get();
-      capturing_device->release();
-    }
-  });
 
   constexpr auto FIRST_TIME = true;
   constexpr auto HIGH_THRESHOLD = 150;
@@ -75,23 +90,20 @@ bool opencv_with_webcams(std::vector<input_device> &connected_webcams) {
   constexpr auto USE_OVERLAY = false;
   constexpr auto WEBCAM_INDEX = 0;
   cv::String overlay_image{};
-  opencv_window window_template(
+  window_template = new opencv_window(
       window_names[0], input_video_devices[0].get(), WEBCAM_INDEX, FIRST_TIME,
       has_webcams[0], video_enabled[0], std::make_pair(0.0, 0.0), USE_CANNY,
       USE_OVERLAY, overlay_image, NO_OVERLAY_ALPHA, LOW_THRESHOLD,
       HIGH_THRESHOLD);
-  constexpr auto SETTINGS_HEIGHT = 280;
-  constexpr auto SETTINGS_WIDTH = 600;
-  constexpr auto SETTINGS_X = 10;
-  constexpr auto SETTINGS_Y = 50;
-  EnhancedWindow settings(SETTINGS_X, SETTINGS_Y, SETTINGS_WIDTH,
-                          SETTINGS_HEIGHT, "Settings");
   cvui::init(&window_names[0], window_names.size());
+}
+
+bool video_gui::loop(std::vector<input_device> &connected_webcams) {
   while (true) {
     main_window(settings, connected_webcams, has_webcams, video_enabled,
                 overlay_enabled, overlay_alpha, overlay_images,
-                window_template);
-    if (window_template.exit_requested()) {
+                *window_template);
+    if (window_template->exit_requested()) {
       return true;
     }
 
@@ -103,11 +115,11 @@ bool opencv_with_webcams(std::vector<input_device> &connected_webcams) {
       auto height_width_pair = height_width_pairs[webcam];
       opencv_window window(
           window_names[webcam], input_video_devices[webcam].get(), webcam,
-          window_template.first_time(), has_webcams[webcam],
-          video_enabled[webcam], height_width_pair, window_template.use_canny(),
-          window_template.use_overlay(), overlay_images[webcam],
-          overlay_alpha[webcam], window_template.low_threshold(),
-          window_template.high_threshold());
+          window_template->first_time(), has_webcams[webcam],
+          video_enabled[webcam], height_width_pair,
+          window_template->use_canny(), window_template->use_overlay(),
+          overlay_images[webcam], overlay_alpha[webcam],
+          window_template->low_threshold(), window_template->high_threshold());
       other_window(window);
       if (window.exit_requested()) {
         return true;
@@ -122,10 +134,15 @@ bool opencv_with_webcams(std::vector<input_device> &connected_webcams) {
       return true;
     }
 
-    window_template.set_first_time(false);
+    window_template->set_first_time(false);
   }
 
   return false;
+}
+
+bool opencv_with_webcams(std::vector<input_device> &connected_webcams) {
+  video_gui gui{connected_webcams};
+  return gui.loop(connected_webcams);
 }
 
 [[noreturn]] void opencv_ui(std::vector<input_device> &connected_webcams) {
