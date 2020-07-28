365cd8cba6f268128dc55b7b23ce6b34adad7b4c
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-28 20:47:30 +0100

    added include config.h
    made sure that all include file start by including config.h

--- video/ui/opencv_ui.h
+++ video/ui/opencv_ui.h
@@ -1,6 +1,17 @@
 #pragma once
 
+#include "config.h"
+
+#if defined(WIN32)
+#pragma warning(push, 0)
+#pragma warning(disable : 4365)
+#endif
+
 #include <vector>
+
+#if defined(WIN32)
+#pragma warning(pop)
+#endif
 
 #include "device/input_device.h"
 
