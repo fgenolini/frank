6d1ecff9b4edd905c8bad1248acf0b3c4285b306
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-28 10:27:42 +0100

    on Windows have both command line and window app
    For the Microsoft Windows platform target, build both a command line application with attached DOS window, and a windowed only application for users to click on from the start menu

--- video/video_main.cpp
+++ video/video_main.cpp
@@ -2,22 +2,11 @@
 #include <cstring>
 #include <iostream>
 
-#include "config.h"
-#ifdef WIN32
-#include <windows.h>
-#endif
-
 #include "run_application.h"
 #include "videoConfig.h"
 
-#ifndef WIN32
 int main(int argc, char const *argv[]) {
   std::cerr << argv[0] << " v" << VIDEO_VERSION_MAJOR << "."
             << VIDEO_VERSION_MINOR << '\n';
   frank::video::run_application(argc, argv);
 }
-#else
-int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-  frank::video::run_application();
-}
-#endif
