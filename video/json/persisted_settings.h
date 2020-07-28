67918e54f09c8ab2e5831c577ca25d0f2a69c54e
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-28 20:48:28 +0100

    Update persisted_settings.h
    include config.h and mute visual C++ warnings

--- video/json/persisted_settings.h
+++ video/json/persisted_settings.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "config.h"
+
+#if defined(WIN32)
+#pragma warning(push, 0)
+#pragma warning(disable : 4365)
+#endif
+
 #include <string>
 #include <map>
+
+#if defined(WIN32)
+#pragma warning(pop)
+#endif
 
 namespace frank::video {
 
