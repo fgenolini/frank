63d43b789d348e7a224f92603904a1f0b8b95c12
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-07-31 13:05:37 +0100

    const correctness

--- video/file/file_dialogs.cpp
+++ video/file/file_dialogs.cpp
@@ -15,7 +15,7 @@ namespace frank::video {
 std::vector<std::string>
 file_dialogs::open_file(std::string const &title,
                         std::string const &default_path,
-                        std::vector<std::string> const &filters) {
+                        std::vector<std::string> const &filters) const {
   std::vector<std::string> output{};
   auto f = pfd::open_file(title, default_path, filters, pfd::opt::force_path);
   for (auto const &name : f.result()) {
--- video/file/file_dialogs.h
+++ video/file/file_dialogs.h
@@ -11,9 +11,9 @@ namespace frank::video {
 
 class file_dialogs {
 public:
-  std::vector<std::string> open_file(std::string const &title,
-                                     std::string const &default_path,
-                                     std::vector<std::string> const &filters);
+  std::vector<std::string>
+  open_file(std::string const &title, std::string const &default_path,
+            std::vector<std::string> const &filters) const;
 };
 
 } // namespace frank::video
