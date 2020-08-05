a9729609c39542f852f4ff3e66973ee9935f713e
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-08-05 13:12:55 +0100

    Update video_main.cpp
    make main testable

--- video/video_main.cpp
+++ video/video_main.cpp
@@ -2,11 +2,20 @@
 #include <cstring>
 #include <iostream>
 
-#include "run_application.h"
 #include "videoConfig.h"
+#include "run_application.h"
+
+#ifdef _TEST_MAIN_2_
+namespace test::frank {
+#endif
 
 int main(int argc, char const *argv[]) {
   std::cerr << argv[0] << " v" << VIDEO_VERSION_MAJOR << "."
             << VIDEO_VERSION_MINOR << '\n';
-  frank::video::run_application(argc, argv);
+  ::frank::video::run_application(argc, argv);
+  return argc;
 }
+
+#ifdef _TEST_MAIN_2_
+} // namespace test::frank
+#endif
