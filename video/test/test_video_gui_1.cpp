69c26be831adc6bdc01f2a48bcf15f35a8041899
François Génolini <2612722+fgenolini@users.noreply.github.com>
2020-08-06 13:18:14 +0100

    clean up

--- video/test/test_video_gui_1.cpp
+++ video/test/test_video_gui_1.cpp
@@ -4,18 +4,9 @@
 #error _TEST_VIDEO_GUI_1_ not defined
 #endif
 
-WARNINGS_OFF
-#include <iostream>
-WARNINGS_ON
-
 #include <catch2/catch.hpp>
-#include <catch2/trompeloeil.hpp>
 
 #include "ui/video_gui.h"
-
-namespace test::frank {} // namespace test::frank
-
-namespace frank::video {} // namespace frank::video
 
 SCENARIO("frank video GUI 1", "[video_gui_1]") {
   GIVEN("Video GUI") {
